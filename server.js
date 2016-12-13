3928ed1f5de0dbaf85df3fe9caef9a226c6ff824
Bruce LANE <bruce@batchass.fr>
2016-12-13 16:16:59 +0100

    ejs

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import config from './config';
+import apiRouter from './api';
 
 import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs');
+
+server.use('/api', apiRouter);
 server.use(express.static('public'));
 
 server.listen(config.port, () => {
