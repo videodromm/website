545cd88d357d238f96006e1e68f08877ba029e0a
Bruce LANE <bruce@batchass.fr>
2017-01-01 23:04:42 +0100

    Eve Porcello and Alex Banks credit

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,7 +4,9 @@ export const Credits = () =>
   <div className="component">
 		<Menu />
     <ul>
-      <li><a target="_blank" href="https://github.com/videodromm">github</a></li>
+      <li><a target="_blank" href="https://github.com/patriciogonzalezvivo/glslEditor">glsl editor by Particio Gonzalez Vivo</a></li>
+      <li><a target="_blank" href="https://github.com/shawnlawson/The_Force/">the_Force by Shawn Lawson</a></li>
+      <li><a target="_blank" href="http://www.moonhighway.com/">Eve Porcello and Alex Banks</a> for their excellent react/nodejs courses on <a target="_blank" href="https://www.linkedin.com/learning/">LinkedIn</a></li>
     </ul>
   </div>
        
