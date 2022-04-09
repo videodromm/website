d69d63edaf54696f512e8d66a2c654e93013a2ab
BruceLane <bruce@batchass.fr>
2022-04-09 11:32:37 +0200

    version

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+//import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin();
+//injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
